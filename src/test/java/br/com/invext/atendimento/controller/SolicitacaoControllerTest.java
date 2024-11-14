b57a6426712d8d7e8fc21e27ebfc31d743d7073e
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2024-11-14 20:21:17 -0300

    Update SolicitacaoControllerTest.java

--- src/test/java/br/com/invext/atendimento/controller/SolicitacaoControllerTest.java
+++ src/test/java/br/com/invext/atendimento/controller/SolicitacaoControllerTest.java
@@ -2,6 +2,7 @@
 
 import br.com.invext.atendimento.model.Solicitacao;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
